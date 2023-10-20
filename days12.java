4920415f5535143a469a966bf6d08538f720cf2f
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-20 18:10:57 +0800

    Create days12.java

---
+++ days12.java
@@ -0,0 +1,17 @@
+package days12;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Operator Perbandingan
+        byte a1,a2;
+        a1 = 2;
+        a2 = 1;
+        // <,>,<=,>=,==,!=.
+        boolean hasil = a1<a2;
+        
+        System.out.println(hasil);
+    }
+    
+}
