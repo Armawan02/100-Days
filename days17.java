1c5a3e30ae5a0fe329a9664b90d1467b63ddb8b4
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-25 22:26:28 +0800

    Create days17.java

---
+++ days17.java
@@ -0,0 +1,20 @@
+package armawan;
+
+public class Days17 {
+    
+    public static void main(String[] args) {
+        
+        String nilai,nila,nil,ni;
+        nilai = "333";
+        nila = "222";
+        nil = "111";
+        
+        byte b1 = Byte.parseByte(nil);
+        int i2 = Integer.parseInt(nilai);
+        double d3 = Double.parseDouble(nila);
+        float f4 = Float.parseFloat(nil);
+        
+        System.out.println(b1+"\n"+i2+"\n"+d3+"\n"+f4);
+    }
+    
+}
