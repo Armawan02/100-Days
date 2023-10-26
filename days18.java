3a5b07179ee932d505009a0df6954615313083a2
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-26 21:59:39 +0800

    Create days18.java

---
+++ days18.java
@@ -0,0 +1,18 @@
+package armawan;
+
+public class Days18 {
+    
+    public static void main(String[] args) {
+        
+        int nilai = 712;
+        double nilai1 = 3.434;
+        
+        String nilaiTeks = Integer.toString(nilai);
+        String nilaiDoubleString = Double.toString(nilai1);
+        
+        System.out.println("##Mahasiswa UNSULBAR 2023##");
+        System.out.println("Jumlah angkatan 2023 : "+nilai1);
+        System.out.println("Kuota penerima KIP-K 2023 : "+nilaiTeks);
+    }
+    
+}
