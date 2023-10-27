d1067150e1d26ec8033f1efbc27c6fb04ab7a968
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-27 19:55:23 +0800

    Create days19.java

---
+++ days19.java
@@ -0,0 +1,29 @@
+package armawan;
+
+public class Days19 {
+    
+    public static void main(String[] args) {
+            //Deklarasi variabel
+        String alamat,noWa;
+        byte saudara;
+        double ipk;
+
+
+        // inisialisasi variabel
+        String nama = "Armawan";
+        byte usia = 19;
+        float  BB = 49.1f;
+        double TB = 160.1;
+        char gender = 'L';
+        System.out.println("Hello friend,my name is "+nama+"\ni am "+usia+"thn"+"\nWeight "+BB+"kg"+"\nHeight "+TB+"cm"+"\ni am gendered "+gender+"\n=========================");
+
+        //Update variabel
+        alamat = "Malunda";
+        noWa = "082336805413";
+        saudara = 3;
+        ipk = 4.0;
+        System.out.println("No wa\t\t : "+noWa+"\nAlamat\t\t : "+alamat+"\nJumlah saudara\t : "+saudara+"\nber ipk\t\t : "+ipk);
+        
+    }
+    
+}
