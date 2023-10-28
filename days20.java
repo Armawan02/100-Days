5a28ff04918547dafe953c2914a37f66eab76f86
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-28 21:29:35 +0800

    Create days20.java

---
+++ days20.java
@@ -0,0 +1,22 @@
+package armawan;
+
+class update {
+    
+    final int pinjaman = 100;
+    final String katakeramat = "Pinjam dulu ";
+}
+
+public class Days20 {
+    
+    public static void main(String[] args) {
+        
+        int pinjaman = 300;
+        String alasan = "Minta dulu ";
+        
+        update pinjam = new update();
+        update Keramat = new update();
+        
+        System.out.println("Agar silaturahmi tidak terputus\n"+Keramat.katakeramat+pinjam.pinjaman);
+    }
+    
+}
