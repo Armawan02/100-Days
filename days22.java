4fe1f9eeeebf215344603ab70f020e160a6783ea
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-30 21:09:14 +0800

    Create days22.java

---
+++ days22.java
@@ -0,0 +1,21 @@
+package armawan;
+
+public class Days22 {
+    
+    public static void main(String[] args) {
+        
+        int a,b,tambah,kurang,kali;
+        double bagi,mod;
+        
+        a = 22;
+        b = 22;
+        
+        tambah = a + b;
+        kurang = a - b;
+        kali   = a * b;
+        bagi   = a / b;
+        mod    = a % b;
+        System.out.println("hasil tambah = "+tambah+"\n"+"hasil kurang = "+kurang+"\n"+"hasil kali = "+kali+"\n"+"hasil bagi = "+bagi+"\n"+"hasil bagi = "+mod);
+    }
+    
+}
