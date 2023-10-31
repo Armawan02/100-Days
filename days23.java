8cdabbf7bef58c46abe78bc0588954e165fd255c
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-10-31 21:02:49 +0800

    Create days23.java

---
+++ days23.java
@@ -0,0 +1,24 @@
+package armawan;
+
+public class Days23 {
+    
+    public static void main(String[] args) {
+        
+        byte a = 13;
+        
+        a += 7; //nilai nya jadi 20
+        System.out.println(a);
+        a -= 5;  //nilai nya jadi 15
+        System.out.println(a);
+        a *= 3;  //nilai nya jadi 45
+        System.out.println(a);
+        a /= a;  //nilai nya jadi 1
+        System.out.println(a);
+        a %= 1;  //nilai nya jadi 0
+        System.out.println(a);
+        
+        /* simple nya, codingan itu terbaca dari atas kebawah
+        dari kiri ke kanan. */
+    }
+    
+}
