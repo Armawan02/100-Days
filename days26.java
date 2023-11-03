86dfe3b9855fc1acbe1f7790751cbde5e809f3da
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-03 18:23:32 +0800

    Create days26.java

---
+++ days26.java
@@ -0,0 +1,17 @@
+package armawan;
+
+public class Days26 {
+    
+    public static void main(String[] args) {
+        
+        byte usia = 20;
+        
+        System.out.println("==Batas Usia Calon Kader Ansor==");
+        if ( usia > 19){
+            System.out.println("Selamat..!, Anda lolos jadi calon kader Ansor");
+        }else {
+            System.out.println("Maaf..!, Anda tidak lolos");
+        }
+    }
+    
+}
