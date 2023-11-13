b8fc17edfbbfbbc1633b57ec5a3ca172d83dc689
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-13 22:07:15 +0800

    Create days36.java

---
+++ days36.java
@@ -0,0 +1,19 @@
+package armawan;
+
+public class Days36 {
+    
+    public static void main(String[] args) {
+        
+        int i, j;
+  for (i=1; i<=5; i++) {
+   for (j=1; j<=i; j++) {
+       
+    System.out.print("A"); 
+   }
+   System.out.println();
+  }
+ 
+
+    }
+    
+}
