715d4406df9bb8609dd62aabb761ddc8eb8a8faa
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-14 23:02:32 +0800

    Create days37.java

---
+++ days37.java
@@ -0,0 +1,21 @@
+package armawan;
+
+public class Days37 {
+    
+    public static void main(String[] args) {
+        
+        byte A,B;
+        boolean a1,a2,a3,a4,a5,a6;
+        A = 13;
+        B = 10;
+        
+        a1 = A == B;
+        a2 = (A != B);
+        a3 = (A > B);
+        a4 = (A < B);
+        a5 = (A >= B);
+        a6 = (A <= B);
+        System.out.println(a1+"\n"+a2+"\n"+a3+"\n"+a4+"\n"+a5+"\n"+a6);
+    }
+    
+}
