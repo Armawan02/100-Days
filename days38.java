7d49d9c9f68ddeb42d026ae616a37f71e192a0d0
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-15 21:57:41 +0800

    Create days38.java

---
+++ days38.java
@@ -0,0 +1,14 @@
+package armawan;
+
+public class Days38 {
+    
+    public static void main(String[] args) {
+//      Membuat Looping pada Array
+        int[] angka = {2, 3, 5, 1, 7, 8, 2, 9};
+        int jumlah = angka.length;
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print(angka[i] + " ");
+        }
+        System.out.println();
+    }
+}
