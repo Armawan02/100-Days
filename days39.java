9a32f10e18bd621113716fb01aa2845bef164cf6
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-16 19:56:05 +0800

    Create days39.java

---
+++ days39.java
@@ -0,0 +1,18 @@
+package armawan;
+
+public class Days39 {
+    
+    public static void main(String[] args) {
+
+        boolean suka = true;
+        String jawaban;
+
+        // menggunakan operator ternary
+        jawaban = suka ? "iya" : "tidak";
+
+        // menampilkan jawaban
+        System.out.println(jawaban);
+
+    }
+    
+}
