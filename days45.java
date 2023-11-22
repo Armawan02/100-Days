a4a66de0562a32d3a69770401021b1f81ad9da88
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-22 22:24:38 +0800

    Create days45.java

---
+++ days45.java
@@ -0,0 +1,14 @@
+package armawan;
+public class Days45 {
+    public static void main(String[] args) {
+        int pil = 1;
+        switch(pil){
+            case 1:
+            System.out.println("laki-laki");
+            break;
+            case 2:
+            System.out.println("perempuan");
+            break;
+        }
+    }
+}
