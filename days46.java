3a52a86bf6816ac0beb54b78c822a445f5dadcf9
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-23 22:02:53 +0800

    Create days46.java

---
+++ days46.java
@@ -0,0 +1,10 @@
+package armawan;
+public class Days46 {
+    public static void main(String[] args) {
+        //Soal membuat looping mencetak nama sbyk 10x
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i+" Armawan");
+            
+        }
+    }
+}
