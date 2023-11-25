cc17cbfb564c54fcb3c02401c9da6fff38e1f7ac
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-25 22:57:20 +0800

    Create days48.java

---
+++ days48.java
@@ -0,0 +1,11 @@
+package armawan;
+public class Days48 {
+    public static void main(String[] args){
+        int angka = 678;
+        String angkaString = Integer.toString(angka);
+        double angkaDouble = 3.147678;
+        String angkaDoubleString = Double.toString(angkaDouble);
+        System.out.println("Nilai\t:"+angkaString);
+        System.out.println("Nilai\t:"+angkaDoubleString);
+    }
+}
