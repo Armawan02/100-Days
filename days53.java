69346fdb4eda803d0ac4b234cc795bee096fd35a
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-11-30 22:38:03 +0800

    Create days53.java

---
+++ days53.java
@@ -0,0 +1,10 @@
+package armawan;
+public class Days53 {
+    public static void saran(String kalimat){
+        System.out.println(kalimat);
+    }
+    public static void main(String[] args) {
+        String kalimat = "makanlah sebelum lapar, berhentilah sebelum kenyang.";
+        saran(kalimat);
+    }
+}
