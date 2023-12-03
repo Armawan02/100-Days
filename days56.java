f1344bdb0666d39bbcf473409e7c72204b6dd4a0
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-03 19:10:29 +0800

    Create days56.java

---
+++ days56.java
@@ -0,0 +1,25 @@
+package armawan;
+public class Days56 {
+    public static void main(String[] args) {
+        Integer a;
+        a = 20+4;
+        System.out.println("hasil tambah = "+a);
+        
+        a = 20-4;
+        System.out.println("hasil kurang = "+a);
+        
+        a = 20*4;
+        System.out.println("Hasil kali = "+a);
+        
+        a = 20/4;
+        System.out.println("Hasil bagi = "+a);
+        
+        a = 20%4;
+        System.out.println("Hasil sisa bagi = "+a);
+    }
+    public static Integer a(int a){
+        int hasil;
+        hasil = a;
+        return hasil;
+    } 
+}
