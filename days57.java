835eec7f3bab05fbd55f66339b3f98f1186e9da2
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-04 21:23:04 +0800

    Create days57.java

---
+++ days57.java
@@ -0,0 +1,14 @@
+package armawan;
+public class Days57 {
+    public static void main (String[] args ) {
+        String nama = "Armawan";
+        char k1 = nama.charAt(0);
+        char k2 = nama.charAt(1);
+        char k3 = nama.charAt(2);
+        char k4 = nama.charAt(3);
+        char k5 = nama.charAt(4);
+        char k6 = nama.charAt(5);
+        char k7 = nama.charAt(6);
+        System.out.println("karakter ke 1 : "+k1+"\nkarakter ke 2 : "+k2+"\nkarakter ke 3 : "+k3+"\nkarakter ke 4 : "+k4+"\nkarakter ke 5 : "+k5+"\nkarakter ke 6 : "+k6+"\nkarakter ke 7 : "+k7);
+    }
+}
