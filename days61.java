cf983b4c2ae8f853279138d1c0b207c38171654f
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-08 19:08:25 +0800

    Create days61.java

---
+++ days61.java
@@ -0,0 +1,7 @@
+package armawan;
+public class Days61 {
+    public static void main(String[] args) {
+        String namaDepan = "Armawan",namaBelakang = " Ome";
+        System.out.println(namaDepan+namaBelakang);
+    }
+}
