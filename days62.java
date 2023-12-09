522df935ce37e48d8a24a9a4983e8077baf78806
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-09 09:39:18 +0800

    Delete days62.java

--- days62.java
+++
@@ -1,9 +0,0 @@
-package armawan;
-public class Days62 {
-    public static void main(String[] args) {
-        String kata = "Nama : Armawan,Lahir : Lekopadis,Kuliah : UNSULBAR";
-        String[] biodata = kata.split(",");
-        String kata1 = biodata[0],kata2 = biodata[1],kata3 = biodata[2];
-        System.out.println(kata1+"\n"+kata2+"\n"+kata3);
-    }
-}
