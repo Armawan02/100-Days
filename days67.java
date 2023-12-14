e27d553a5e1cc4f7ab255000914c6f6c47b6576f
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-14 16:31:11 +0800

    Create days67.java

---
+++ days67.java
@@ -0,0 +1,12 @@
+package armawan;
+public class Days67 {
+    public static void main(String[] args) {
+         
+        StringBuffer sb = new StringBuffer("Harmawan");
+        System.out.println("before : " + sb);
+        sb.deleteCharAt(0);
+        System.out.println("after : " + sb);
+
+       System.out.println(" _____\n√sakit = harapan");
+    }
+}
