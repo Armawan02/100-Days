f6c95ec404f73f8d9d77ec4760fbf2743f1e1f09
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-18 11:55:28 +0800

    Create days71SetterdanGetter.java
    Berisi 2 Program, materi Object Oriendted Programing

---
+++ days71SetterdanGetter.java
@@ -0,0 +1,24 @@
+package armawan;
+public class Main {
+     public String nama,nim;
+    public int umur;
+    
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+    public int getUmur(){
+        return umur;
+    }
+    public void setUmur(int umur){
+        this.umur = umur;
+    }
+}
