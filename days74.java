6eeed1709ec0ffcd8e636b441d569e9f3048c9d5
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2023-12-21 22:30:08 +0800

    Create days74.java

---
+++ days74.java
@@ -0,0 +1,21 @@
+package armawan;
+
+          class mentoringMerryDanKhaeril100DaysCoding {
+        String a,b,c,d,e;
+              
+        void showName(){
+            System.out.println("NAMA MENTI MENTORING MERRY & KHAERIL\n\n1. "+a+"\n2. "+b+"\n3. "+c+"\n4. "+d+"\n5. "+e);
+        }
+    }
+    public class Days74{
+        
+        public static void main(String[] args) {
+            mentoringMerryDanKhaeril100DaysCoding namaMenti = new mentoringMerryDanKhaeril100DaysCoding();
+            namaMenti.a= "Armawan";
+            namaMenti.b= "Baharuddin";
+            namaMenti.c= "Futri Adriana Aksar";
+            namaMenti.d= "Nurmadinah";
+            namaMenti.e= "Nur Rahiman";
+            namaMenti.showName();
+        }
+    }
