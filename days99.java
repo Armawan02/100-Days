ebf91e4be1d01a684d572bb3b2a6e6f3f195f390
Armawan02 <147232972+Armawan02@users.noreply.github.com>
2024-01-15 21:37:55 +0800

    Create days99.java
    Membuat biodata palsu

---
+++ days99.java
@@ -0,0 +1,11 @@
+package armawan;
+
+public class Days99 {
+    
+    public static void main(String[] args) {
+        
+        String nama = "Ome",pekerjaan = "Menguras Air Laut",StatusPerkawinan = "Duda perjaka";
+        byte umur = 30;
+        System.out.println("Biodata Palsu"+"\nnama : "+nama+"\nPekerjaan : "+pekerjaan+"\nStatus Perkawinan : "+StatusPerkawinan+"\nUsia : "+umur+" Tahun");
+    }
+}
